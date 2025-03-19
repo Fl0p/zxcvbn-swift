dfac79ace2ce6c7c28ba8bc2057b94cb8f45e479
Flop Butylkin <sergej.khlopenov@paysera.net>
2025-03-19 16:44:32 +0100

    Update Match for Public

--- Sources/zxcvbn/Match.swift
+++ Sources/zxcvbn/Match.swift
@@ -7,70 +7,73 @@
 
 typealias MatcherBlock = (String) -> [Match]
 
-class Match {
-    var pattern: String = ""
-    var i: Int = 0
-    var j: Int = 0
-    var token: String = ""
-    var entropy: Float = 0.0
-    var cardinality: Int = 0
-    var guesses: Double? = nil
-    var guessesLog10: Double? = nil
-
-    // Dictionary Match
-    var matchedWord: String? = nil
-    var dictionaryName: String? = nil
-    var rank: Double? = nil
-    var baseEntropy: Float? = nil
-    var upperCaseEntropy: Float? = nil
-
-    // L33t Match
-    var l33t: Bool? = nil
-    var sub: [Character: Character]? = nil
-    var subDisplay: String? = nil
-    var l33tEntropy: Float? = nil
-
-    // Spatial Match
-    var graph: String? = nil
-    var turns: Int? = nil
-    var shiftedCount: Int? = nil
-
-    // Repeat Match
-    var repeatedChar: String? = nil
-    var repeatCount: Double? = nil
-    var baseToken: String? = nil
-    var baseMatches: [Match]? = nil
-
-    // Sequence Match
-    var sequenceName: String? = nil
-    var sequenceSpace: Int? = nil
-    var ascending: Bool? = nil
-
-    // Date Match
-    var year: Int? = nil
-    var month: Int? = nil
-    var day: Int? = nil
-    var separator: String? = nil
-
-    // Dictionary Guesses
-    var baseGuesses: Double? = nil
-    var uppercaseVariations: Double? = nil
-    var l33tVariations: Double? = nil
-    var reversed: Bool? = nil
-
-    // Regex Match
-    var regexName: String? = nil
-    var regexMatch: [String]? = nil
+public class Match {
+    public var i: Int = 0
+    public var j: Int = 0
+    public var token: String = ""
 
     public init(i: Int, j: Int, token: String) {
         self.i = i
         self.j = j
         self.token = token
     }
+
+    // Pattern Match
+    public var pattern: String = ""
+    public var entropy: Float = 0.0
+    public var cardinality: Int = 0
+    public var guesses: Double? = nil
+    public var guessesLog10: Double? = nil
+
+    // Dictionary Match
+    public var matchedWord: String? = nil
+    public var dictionaryName: String? = nil
+    public var rank: Double? = nil
+    public var baseEntropy: Float? = nil
+    public var upperCaseEntropy: Float? = nil
+
+    // L33t Match
+    public var l33t: Bool? = nil
+    public var sub: [Character: Character]? = nil
+    public var subDisplay: String? = nil
+    public var l33tEntropy: Float? = nil
+
+    // Spatial Match
+    public var graph: String? = nil
+    public var turns: Int? = nil
+    public var shiftedCount: Int? = nil
+
+    // Repeat Match
+    public var repeatedChar: String? = nil
+    public var repeatCount: Double? = nil
+    public var baseToken: String? = nil
+    public var baseMatches: [Match]? = nil
+
+    // Sequence Match
+    public var sequenceName: String? = nil
+    public var sequenceSpace: Int? = nil
+    public var ascending: Bool? = nil
+
+    // Date Match
+    public var year: Int? = nil
+    public var month: Int? = nil
+    public var day: Int? = nil
+    public var separator: String? = nil
+
+    // Dictionary Guesses
+    public var baseGuesses: Double? = nil
+    public var uppercaseVariations: Double? = nil
+    public var l33tVariations: Double? = nil
+    public var reversed: Bool? = nil
+
+    // Regex Match
+    public var regexName: String? = nil
+    public var regexMatch: [String]? = nil
+
 }
 
 extension Match: Equatable {
-    static func == (lhs: Match, rhs: Match) -> Bool {
+    public static func == (lhs: Match, rhs: Match) -> Bool {
         return lhs.pattern == rhs.pattern &&
             lhs.i == rhs.i &&
             lhs.j == rhs.j &&
